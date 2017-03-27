cadad97eed404a71735b276d937f60528669e3c0
neelin21 <neelinvakil@Neelins-MacBook-Pro-2.local>
2017-03-27 17:59:02 -0400

    Added file to handle creating threads for the system

---
+++ Robot/Threading.py
@@ -0,0 +1,12 @@
+#This file will be used to create the threading structure
+#Neelin Vakil
+#March 2017
+
+import threading
+import time
+
+def ThreadMaking():
+    print "Starting Threads..."
+    
+    
+    return
