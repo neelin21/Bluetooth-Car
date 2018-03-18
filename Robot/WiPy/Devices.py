ff1c148c7414ac47583fdf89c77246d7501e2095
neelin21 <neelinvakil@Neelins-MacBook-Pro-2.local>
2018-03-18 19:21:25 -0400

    Added I2C code
    Added basic setup for I2C communication. Functions will later be added
    based on connected devices.

--- Robot/WiPy/Devices.py
+++ Robot/WiPy/Devices.py
@@ -3,9 +3,10 @@
 # Description: This file contains codes to interface with devices connected via I2C
 # Author: Neelin Vakil
 #####################################################
+from machine import I2C
 
 class Devices(object):
 
     def __init__(self):
-
+        self.i2c = I2C(0, I2C.MASTER, baudrate=100000)
         return
