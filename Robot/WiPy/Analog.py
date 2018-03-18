79cf620e310bc2d8156a6dee1b50947fc6d153b4
neelin21 <neelinvakil@Neelins-MacBook-Pro-2.local>
2018-03-18 19:16:24 -0400

    Added classes for Analogs
    Created input and output classes and made the initializations for each.
    Functions will be added later based on their use.

--- Robot/WiPy/Analog.py
+++ Robot/WiPy/Analog.py
@@ -5,12 +5,24 @@
 # Author: Neelin Vakil
 #####################################################
 from machine import ADC
+from machine import DAC
 
-class Analog(object):
+class Analog_Inputs(object):
 
     def __init__(self):
-
         adc = ADC(0)
         self.input_1 = adc.channel(pin='P13', attn=ADC.ATTN_11DB)
+        self.input_2 = adc.channel(pin='P14', attn=ADC.ATTN_11DB)
+        self.input_3 = adc.channel(pin='P15', attn=ADC.ATTN_11DB)
+        self.input_4 = adc.channel(pin='P16', attn=ADC.ATTN_11DB)
+        self.input_5 = adc.channel(pin='P17', attn=ADC.ATTN_11DB)
+        self.input_6 = adc.channel(pin='P18', attn=ADC.ATTN_11DB)
 
+        return
+
+class Analog_Outputs(object):
+
+    def __init__(self):
+        self.dac_1 = DAC('P21')
+        self.dac_2 = DAC('P22')
         return
